fddab70b80c93ced7ca6af0f95d0abe4a512f6ca
Samarth <samarth1974@gmail.com>
2022-06-10 22:59:53 +0530

    Last Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='flex items-center bg-teal-500 w-full h-16'>
+        <h1 className='text-center text-white text-2xl mx-auto font-mono'>Pixabay Image Search</h1>
+    </div>
+  )
+}
